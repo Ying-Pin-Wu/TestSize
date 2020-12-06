b21acec3e72babccafd3ed1de39562631637fe9c
Ying-Pin Wu <marcuswu.ece@gmail.com>
2020-12-06 18:56:34 +0800

    加入專案檔案。

---
+++ Openfile/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+unsigned int i = 0;
+size_t ret;
+int FIND_EOF = 0;
+char j = 0;
+int main()
+{
+	cout << "¾Þ§A¶Üªº " << 0x12<<endl;
+	cout << "Size of char : " << sizeof(char) << endl;
+	cout << "Size of int : " << sizeof(int) << endl;
+	cout << "Size of short int : " << sizeof(short int) << endl;
+	cout << "Size of long int : " << sizeof(long int) << endl;
+	cout << "Size of float : " << sizeof(float) << endl;
+	cout << "Size of double : " << sizeof(double) << endl;
+	cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+	return 0;
+}
