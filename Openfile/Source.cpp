6bca5124772b1d6e897ef411a6d84f7e7bb54ee3
Ying-Pin Wu <marcuswu.ece@gmail.com>
2020-12-13 16:45:42 +0800

    1213:04:40 A version

--- Openfile/Source.cpp
+++ Openfile/Source.cpp
@@ -5,11 +5,10 @@
 using namespace std;
 unsigned int i = 0;
 size_t ret;
-int FIND_EOF = 0;
 char j = 0;
 int main()
 {
-	cout << "¾Þ§A¶Üªº " << 0x12<<endl;
+	cout << "AAAA verision X " << 0x22<<endl;
 	cout << "Size of char : " << sizeof(char) << endl;
 	cout << "Size of int : " << sizeof(int) << endl;
 	cout << "Size of short int : " << sizeof(short int) << endl;
@@ -17,5 +16,8 @@ int main()
 	cout << "Size of float : " << sizeof(float) << endl;
 	cout << "Size of double : " << sizeof(double) << endl;
 	cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+	
+	system("pause");
+
 	return 0;
 }
